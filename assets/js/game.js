2ab5f25e414a307b71eca4ba180dc79132d74ffa
“Gigi <debortolg@gmail.com>
2022-08-08 17:32:59 -0700

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -66,7 +66,53 @@ var fight = function (enemyName) {
         }
     } // end of while loop
 }; // end of fight function
+// Shop Function
 
+var shop = function() {
+    // ask player what they'd like to do
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    )
+    // use switch to carry our action
+    switch (shopOptionPrompt) {
+    case "REFILL":
+    case "refill":
+        if (playerMoney >= 7) {
+            window.alert("Refilling player's health by 20 for 7 dollars.");
+
+        // increase health and decrease money
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+        } else {
+            window.alert("You don't have enough money");
+    }
+        break;
+    case "UPGRADE":
+    case "upgrade":
+        if (playerMoney >= 7) {
+            window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+        // increase attack and decrease money
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+        } else {
+            window.alert("You don't have enough money");
+    }
+        break; 
+    case "LEAVE":
+    case "leave":
+            window.alert("Leaving the store.");
+        
+        // do nothing, so function will end
+        break;
+    default:
+            window.alert("You did not pick a valid option. Try again.");
+
+        // call shop() again to force player to pick a valid option
+        shop();
+        break;
+    };
+}
 // function to start a new game (wrapping prev for loop in start game function)
 var startGame = function () {
     for (var i = 0; i < enemyNames.length; i++) {
@@ -90,6 +136,15 @@ var startGame = function () {
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
+            // if we're not at the last enemy in the array
+            if (playerHealth > 0 && enemyNames.length - 1) {
+                // ask if player wants to use the store before next round
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+                // if yes, take them to the store() function
+                if (storeConfirm) {
+                    shop();
+                }  
+            }
         }
         // if player isn't alive, stop the game
         else {
@@ -105,12 +160,12 @@ startGame();
 var endGame = function () {
     // If player is still alive, player wins!
     if (playerHealth > 0) {
-        window.alert("Great job, you've survived the game@ now you have a score of " + playerMoney + ".");
+        window.alert("Great job, you've survived the game now you have a score of " + playerMoney + ".");
     } else {
         window.alert("You've lost your robot in battle.");
     }
     // playAgainConfirm is an example of a local scope that is undefined outside the function
-var playAgainConfirm = window.confirm("would you like to play again?");
+var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
         // Restart game
@@ -121,3 +176,4 @@ var playAgainConfirm = window.confirm("would you like to play again?");
 }
 // Run endGame function
 endGame();
+
